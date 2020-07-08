f6523bcd3ffc58695967289576bd84e98cb6c0d5
Edo <edo.g@witchery.io>
2020-07-08 16:14:38 +0400

    init firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,14 @@ export const environment = {
 };
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBkVooXTbNWG2yFGMeyWGsbd2ego7tUhfM",
-  authDomain: "planning-list-d77af.firebaseapp.com",
-  databaseURL: "https://planning-list-d77af.firebaseio.com",
-  projectId: "planning-list-d77af",
-  storageBucket: "planning-list-d77af.appspot.com",
-  messagingSenderId: "87475809286",
-  appId: "1:87475809286:web:2445cb9af6f708304e2787",
-  measurementId: "G-PBHHX4Z47T"
+  apiKey: 'AIzaSyBkVooXTbNWG2yFGMeyWGsbd2ego7tUhfM',
+  authDomain: 'planning-list-d77af.firebaseapp.com',
+  databaseURL: 'https://planning-list-d77af.firebaseio.com',
+  projectId: 'planning-list-d77af',
+  storageBucket: 'planning-list-d77af.appspot.com',
+  messagingSenderId: '87475809286',
+  appId: '1:87475809286:web:2445cb9af6f708304e2787',
+  measurementId: 'G-PBHHX4Z47T'
 };
 
 /*
